fd6717d1f0457dd61b8722177cc2e21be5832721
johan.kok <johan.kok@grabtaxi.com>
2019-09-20 10:58:28 +0800

    Refractored protobuf message declaration - Abstracted repeated message patterns out for resuability - created nodeProperties message to store address / port of node

--- CS6203/src/group-project/Raft/raft.go
+++ CS6203/src/group-project/Raft/raft.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
-func getLeader() (string, int, error) {
+/*
+State definitions
+0 - Follower
+1 - Candidate
+2 - Leader
+*/
+const (
+	timeout = 1000 // ms
+	state 	= 0;
+)
+
+func requestLeader() (string, int, error) {
 	/*
 		Entry point for RAFT. Nodes will start as slaves until timeout before moving to candidate state.
 	*/
---
+++ CS6203/src/group-project/Raft/server.go
@@ -0,0 +1,9 @@
+package Raft
+
+import (
+	pb "group-project/Protobuf/Generate"
+)
+
+type Server struct {}
+
+func (s *Server)
