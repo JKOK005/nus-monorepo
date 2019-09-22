521d3ba01b3375ed9c6cd806a0c35b4f49a99da8
johan.kok <johan.kok@grabtaxi.com>
2019-09-22 21:36:00 +0800

    Fixed part 1 logic bug - Supposed to take the min of a word if it is present in a doc rather than taking the total sum of all words

--- CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part1.scala
+++ CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part1.scala
@@ -1,9 +1,12 @@
 package com.CS5344.lab_3
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import commons.Utils
+import java.io.File
+
+import scala.collection.mutable.ListBuffer
 
 object part1 extends App {
   def initSparkContext(applicationName: String): SparkSession = {
@@ -13,17 +16,35 @@ object part1 extends App {
 
   implicit val spark: SparkSession = initSparkContext("CS5344-lab-3-part-1")
 
-  val dataDf      = spark.read.textFile("./src/main/resources/datafiles").toDF("words")
-  val stopWordsDf = spark.read.textFile("./src/main/resources/stopwords.txt").toDF("words")
+  val dataFileDir       = "./src/main/resources/datafiles"
+  val stopWordsFilePath = "./src/main/resources/stopwords.txt"
 
-  val wordsDf     = Utils.dfToWordsVector(dataDf)
-  val filteredDf  = Utils.dfRemoveStopWords(wordsDf, stopWordsDf)
+  val stopWordsDf   = spark.read.textFile(stopWordsFilePath)
+                          .toDF("words")
+                          .withColumn("words", lower(col("words")))
+                          .cache
 
-  val aggregatedDf  = filteredDf.groupBy("col").count() // Aggregate by word and count
-  aggregatedDf.orderBy(desc("count"))
-              .coalesce(1)
-              .write
-              .mode("Overwrite")
-              .option("header","true")
-              .csv("./src/main/resources/output/part1") // Order by count and write to resources/output
+  val allAggregatedDfLst  = new ListBuffer[DataFrame]()
+  val dataFilePtr   = new File(dataFileDir)
+
+  dataFilePtr.listFiles.foreach(
+    filePath => {
+      val dataDf              = spark.read.textFile(filePath.toString).toDF("words")
+      val singleWordDf        = Utils.dfToWordsVector(dataDf)
+      val removeStopWordsDf   = Utils.dfRemoveStopWords(singleWordDf, stopWordsDf)
+      val aggregatedDf        = removeStopWordsDf.groupBy("col").count.cache  // Aggregate by word and count
+      allAggregatedDfLst.append(aggregatedDf)
+    }
+  )
+
+  val finalDf = allAggregatedDfLst.reduce(_.union(_))
+                                  .groupBy("col")
+                                  .min("count")
+                                  .orderBy(desc("min(count)"))
+
+  finalDf.coalesce(1)
+          .write
+          .mode("Overwrite")
+          .option("header","true")
+          .csv("./src/main/resources/output/part1") // Order by count and write to resources/output
 }
--- CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
+++ CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
@@ -1,5 +1,14 @@
 package com.CS5344.lab_3
 
-object part2 extends App {
+import com.CS5344.lab_3.part1.initSparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
+object part2 extends App {
+  def initSparkContext(applicationName: String): SparkSession = {
+    val sparkConfig = new SparkConf().setAppName(applicationName)
+    SparkSession.builder().config(sparkConfig).enableHiveSupport().getOrCreate()
+  }
+
+  implicit val spark: SparkSession = initSparkContext("CS5344-lab-3-part-1")
 }
