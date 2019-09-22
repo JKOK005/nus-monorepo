a1b8cef705821567c47c7f195d1445c37c3e55c8
johan.kok <johan.kok@grabtaxi.com>
2019-09-22 23:01:54 +0800

    Refractored code: - Must ensure that words selected are common across all documents

--- CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
+++ CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
@@ -1,8 +1,10 @@
 package com.CS5344.lab_3
 
-import com.CS5344.lab_3.part1.initSparkContext
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import commons.Utils
+import java.io.File
 
 object part2 extends App {
   def initSparkContext(applicationName: String): SparkSession = {
@@ -10,5 +12,13 @@ object part2 extends App {
     SparkSession.builder().config(sparkConfig).enableHiveSupport().getOrCreate()
   }
 
-  implicit val spark: SparkSession = initSparkContext("CS5344-lab-3-part-1")
+  implicit val spark: SparkSession = initSparkContext("CS5344-lab-3-part-2")
+
+  val dataFileDir       = "./src/main/resources/datafiles"
+  val stopWordsFilePath = "./src/main/resources/stopwords.txt"
+
+  val stopWordsDf       = spark.read.textFile(stopWordsFilePath)
+                                .toDF("words")
+                                .withColumn("words", lower(col("words")))
+                                .cache
 }
