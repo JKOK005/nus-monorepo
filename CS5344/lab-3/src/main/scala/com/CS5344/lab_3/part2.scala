43aa8a6db3616590d56429b75b56bbd148c52665
johan.kok <johan.kok@grabtaxi.com>
2019-09-23 00:38:26 +0800

    Partial completion of part 2 of assignment

--- CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
+++ CS5344/lab-3/src/main/scala/com/CS5344/lab_3/part2.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import commons.Utils
 import java.io.File
+import scala.collection.mutable.ListBuffer
 
 object part2 extends App {
   def initSparkContext(applicationName: String): SparkSession = {
@@ -12,13 +13,43 @@ object part2 extends App {
     SparkSession.builder().config(sparkConfig).enableHiveSupport().getOrCreate()
   }
 
+  def getTF_IDF(df: DataFrame, dictionaryDf: DataFrame, wordCount: Long, numDoc: Int): DataFrame = {
+    df.join(dictionaryDf, df("col") === dictionaryDf("col") ,"inner")
+      .withColumn("tf_idf", (df("count") / wordCount) * (log(lit(numDoc) / dictionaryDf("count"))))
+  }
+
   implicit val spark: SparkSession = initSparkContext("CS5344-lab-3-part-2")
 
   val dataFileDir       = "./src/main/resources/datafiles"
   val stopWordsFilePath = "./src/main/resources/stopwords.txt"
 
-  val stopWordsDf       = spark.read.textFile(stopWordsFilePath)
+  val stopWordsDf = spark.read.textFile(stopWordsFilePath)
                                 .toDF("words")
                                 .withColumn("words", lower(col("words")))
                                 .cache
+
+  val singleDocDf   = new ListBuffer[DataFrame]()   // To track information for a single document
+  val allDocDf      = new ListBuffer[DataFrame]()   // To track aggregated information across all documents
+  val dataFilePtr   = new File(dataFileDir)
+
+  dataFilePtr.listFiles.foreach(
+    filePath => {
+      val dataDf              = spark.read.textFile(filePath.toString).toDF("words")
+      val singleWordDf        = Utils.dfToWordsVector(dataDf)
+      val removeStopWordsDf   = Utils.dfRemoveStopWords(singleWordDf, stopWordsDf)
+
+      singleDocDf.append(removeStopWordsDf.groupBy("col").count)
+      allDocDf.append(removeStopWordsDf.withColumn("path", lit(filePath.toString)))
+    }
+  )
+
+  // Compute dictionary of all words and the number of documents that contains them
+  val dictionaryDf = allDocDf.reduce(_.union(_))
+                            .distinct
+                            .groupBy("col")
+                            .count.cache
+
+  // Computes TF-IDF value of for each document
+
+
 }
