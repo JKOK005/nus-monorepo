d3481d5a32af1a928da4a584fa8e1a0657f3d10c
johan.kok <johan.kok@grabtaxi.com>
2019-10-30 18:26:51 +0800

    Added new functions and TODOs for client.go

--- CS6203/src/group-project/Services/Client/Client.go
+++ CS6203/src/group-project/Services/Client/Client.go
@@ -7,6 +7,25 @@ import (
 	"group-project/Utils"
 )
 
+func (c *Client) hashKey(key string) uint32 {
+	/*
+	Applies a hash function over a key
+	Returns the hashed value of the key to represent the hash group it should be assigned to
+	*/
+}
+
+func (c *Client) validate(key string) (bool, string) {
+	/*
+	Validates the hash group the key should be assigned to via CHORD
+	We first hash the raw key to obtain a hash group number.
+	We then query the Chord manager to determine if the key should be present locally or if it should be dispatched to a different address
+
+	TODO: Implement return type of chord manager once @Johnfietsen has merged his impelmentation of chord to master
+	TODO: Return types (bool, string) -> (is local node, address to be sent to if local node = False). These return types are merely a proxy and can change
+	*/
+	return nil, nil
+}
+
 func (c *Client) PutKey(ctx context.Context, msg *pb.PutKeyMsg) (*pb.PutKeyResp, error) {
 	/*
 	TODO: Check that the request is a legit request for the node, else we forward it to somewhere else
