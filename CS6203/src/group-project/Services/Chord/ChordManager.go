09fe587b0578485bd815efffc096ef246238fca5
Johnfietsen <31688199+Johnfietsen@users.noreply.github.com>
2019-10-31 21:27:20 +0800

    Repair pr (#9)
    * added Chord files
    
    * adjusted main and ChannelsHelper

---
+++ CS6203/src/group-project/Services/Chord/ChordManager.go
@@ -0,0 +1,102 @@
+package Chord
+
+import (
+	util "group-project/Utils"
+
+	"fmt"
+	"github.com/golang/glog"
+)
+
+type ChordManager struct {
+	NodeAddr		string			// Node address
+	NodePort		uint32			// Node port
+	BaseHashGroup	uint32			// Base hash number for a group
+	NrSuccessors	uint32			// Number of entries in finger table
+	FingerTable		*FingerTable	// Struct containing node info of successors
+	HighestHash		uint32			// Highest possible hash value
+}
+
+func (c *ChordManager) searchFingerTable(baseHashGroupSearched uint32) util.ChannelsNodeInfo {
+	/*
+		Receives a bashashgroup it needs to find
+		Iterates through the finger table and looks for closest hash
+		Returns the node information of the server with the closest hash
+	*/
+	smallestDiff := uint32(1e9)
+	var closestHash uint32
+	var diff uint32
+	var closestSuccessor util.ChannelsNodeInfo
+
+	for baseHashGroup, successor := range c.FingerTable.Successors {
+		if baseHashGroupSearched >=  baseHashGroup {
+			diff = baseHashGroupSearched - baseHashGroup
+		} else {
+			// Correct for circular shape of hashing
+			diff = baseHashGroupSearched + (c.HighestHash - baseHashGroup)
+		}
+
+		if diff == 0 {
+			// Hash found
+			closestHash = baseHashGroup
+			glog.Infof(fmt.Sprint("Found hashgroup ", closestHash, " in table"))
+			return successor
+		} else if diff < smallestDiff {
+			// New closest hash found
+			closestHash = baseHashGroup
+			closestSuccessor = successor
+			glog.Infof(fmt.Sprint("Found closer hash ", closestHash))
+		}
+		closestSuccessor = successor
+	}
+	glog.Infof(fmt.Sprint("Redirecting to ", closestHash))
+
+	return closestSuccessor
+}
+
+
+func (c *ChordManager) search(baseHashGroupSearched uint32) util.ChannelsNodeInfo {
+	/*
+		Receives a bashashgroup it needs to find
+		Checks if this server contains the hash, checks finger table otherwise
+		Returns the node information of the server with the closest hash
+	*/
+	glog.Infof(fmt.Sprint("Searching for ", baseHashGroupSearched, " from ",
+						  c.BaseHashGroup))
+	var closestSuccessor util.ChannelsNodeInfo
+	// Checks its own hash
+	if baseHashGroupSearched == c.BaseHashGroup {
+		glog.Infof("Found hashgroup here")
+		nodeObj := util.ChannelsNodeInfo{Addr: c.NodeAddr, Port: c.NodePort}
+		closestSuccessor = nodeObj
+	} else {
+		closestSuccessor = c.searchFingerTable(baseHashGroupSearched)
+	}
+	return closestSuccessor
+}
+
+func (c *ChordManager) Routing() {
+	/*
+		Routine that constantly checks ChordRoutingChannel.ReqCh
+		If there is a request, returns the nodeinfo of closest server
+	*/
+	for {
+		select {
+		case baseHashGroupSearched := <-util.ChordRoutingChannel.ReqCh:
+			closestSuccessor := c.search(baseHashGroupSearched)
+			go func() {
+				util.ChordRoutingChannel.RespCh <- closestSuccessor
+			}()
+			glog.Info(fmt.Sprint("Closest server ",
+								 <-util.ChordRoutingChannel.ReqCh))
+			glog.Info(fmt.Sprint(closestSuccessor))
+		default:
+		}
+	}
+}
+
+func (c ChordManager) Start() {
+	c.FingerTable, _ = NewFingerTable(c.NodeAddr, c.NodePort, c.NrSuccessors,
+									  c.BaseHashGroup, c.HighestHash)
+	c.FingerTable.FillTable()
+	c.Routing()
+}
---
+++ CS6203/src/group-project/Services/Chord/FingerTable.go
@@ -0,0 +1,130 @@
+package Chord
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"sort"
+	"math"
+	util "group-project/Utils"
+	"github.com/golang/glog"
+)
+
+type FingerTable struct {
+	MyInfo			*util.ChannelsNodeInfo				// Node info
+	NrSuccessors	uint32								// Number of entries in finger table
+	Successors		map[uint32]util.ChannelsNodeInfo	// Entries in finger table
+	BaseHashGroup 	uint32								// Base hash number for a group
+	HighestHash		uint32								// Highest possible hash value
+}
+
+var (
+	zkCli 			*util.SdClient
+)
+
+
+func NewFingerTable(myAddr string, myPort uint32, nrSuccessors uint32,
+					baseHashGroup uint32, highestHash uint32) (*FingerTable,
+																		error) {
+	/*
+		Creates a new FingerTable struct and returns to the user
+		Also initializes node path in Zookeeper
+	*/
+	if zookeeperCli, err := util.NewZkClient(); err != nil {
+		glog.Error(err)
+		return nil, err
+	} else {
+		glog.Infof("Building finger table %s:%d", myAddr, myPort)
+		nodeObj := &util.ChannelsNodeInfo{Addr: myAddr, Port: myPort}
+		data, _ := json.Marshal(nodeObj)
+		glog.Info(string(data))
+		err = zookeeperCli.RegisterEphemeralNode(zookeeperCli.
+					PrependNodePath(fmt.Sprintf("%d/", baseHashGroup)), data)
+		if err != nil {
+			return nil, err
+		}
+		zkCli = zookeeperCli // Cache client
+		return &FingerTable{MyInfo: nodeObj, NrSuccessors: nrSuccessors,
+							Successors: make(map[uint32]util.ChannelsNodeInfo),
+							BaseHashGroup: baseHashGroup,
+							HighestHash: highestHash}, nil
+	}
+}
+
+
+func (f *FingerTable) findSuccessor(baseHashGroupsInt []uint32, value uint32,
+									successors map[uint32]util.ChannelsNodeInfo) bool {
+	/*
+		Iterate through list of baseHashGroups
+		Check if the correct hash is found and add to the successors
+	*/
+	for _, eInt := range baseHashGroupsInt {
+		if eInt == value {
+			nodePath, _ := zkCli.GetNodePaths(zkCli.
+									PrependNodePath(fmt.Sprintf("%d", eInt)))
+			nodeData, _ := zkCli.GetNodeValue(zkCli.PrependNodePath(fmt.
+										Sprintf("%d/%s", eInt, nodePath[0])))
+			nodeInfo := new(util.ChannelsNodeInfo)
+			json.Unmarshal(nodeData, nodeInfo)
+			successors[eInt] = *nodeInfo
+			return true
+		}
+	}
+	return false
+}
+
+
+func (f *FingerTable) chooseSuccessors(baseHashGroupsInt []uint32) {
+	/*
+		Finds baseHashGroup closest to the baseHashGroup needed
+		Calls findSuccessor to add it to the list
+	*/
+	for i := uint32(0); i < f.NrSuccessors; i++ {
+		value := f.BaseHashGroup + uint32(math.Pow(2, float64(i)))
+		found := false
+		for found == false {
+			if value > f.HighestHash {
+				value = value % f.HighestHash
+			}
+			found = f.findSuccessor(baseHashGroupsInt, value, f.Successors)
+			value = value + 1
+		}
+	}
+	glog.Info(fmt.Sprint("Successors ", f.Successors, " of ", f.BaseHashGroup))
+}
+
+
+func (f *FingerTable) FillTable() {
+	/*
+		Finds which baseHashGroups are populated (from Zookeeper)
+		Choose successors to add to the finger table
+	*/
+	baseHashGroupsFound, _ := zkCli.GetNodePaths("/nodes")
+	var baseHashGroupsPopulated []string
+
+	for _, baseHashGroup := range baseHashGroupsFound {
+		nodesFound, _ := zkCli.GetNodePaths(fmt.Sprint("/nodes/",
+													   baseHashGroup))
+		if len(nodesFound) > 0 {
+			baseHashGroupsPopulated = append(baseHashGroupsPopulated,
+											 baseHashGroup)
+		}
+	}
+
+	var baseHashGroupsInt []uint32
+	for _, eStr := range baseHashGroupsPopulated {
+		eInt, _ := strconv.ParseUint(eStr, 10, 32)
+		baseHashGroupsInt = append(baseHashGroupsInt, uint32(eInt))
+	}
+	sort.Slice(baseHashGroupsInt, func(i,
+		j int) bool { return baseHashGroupsInt[i] < baseHashGroupsInt[j] })
+	glog.Info("BaseHashGroups populated: ", baseHashGroupsInt)
+
+	f.chooseSuccessors(baseHashGroupsInt)
+}
+
+
+func (f *FingerTable) ServerLeaving(baseHashGroup uint32) {
+	// TODO add new hashgroup to finger table
+	delete(f.Successors, baseHashGroup)
+}
