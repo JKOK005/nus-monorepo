1674589be4b456cb6a5544ded0ab253ccecb25ed
johan.kok <johan.kok@grabtaxi.com>
2019-10-31 23:27:54 +0800

    Partial implementation of GetKey / PutKey functions

--- CS6203/src/group-project/Services/Client/Client.go
+++ CS6203/src/group-project/Services/Client/Client.go
@@ -7,42 +7,43 @@ import (
 	"group-project/Utils"
 )
 
-func (c *Client) hashKey(key string) uint32 {
+func (c *Client) locate(key string) Utils.ChannelsNodeInfo {
 	/*
-	Applies a hash function over a key
-	Returns the hashed value of the key to represent the hash group it should be assigned to
-	*/
-}
-
-func (c *Client) validate(key string) (bool, string) {
-	/*
-	Validates the hash group the key should be assigned to via CHORD
+	Determines the hash group the key should be located at via CHORD
 	We first hash the raw key to obtain a hash group number.
 	We then query the Chord manager to determine if the key should be present locally or if it should be dispatched to a different address
-
-	TODO: Implement return type of chord manager once @Johnfietsen has merged his impelmentation of chord to master
-	TODO: Return types (bool, string) -> (is local node, address to be sent to if local node = False). These return types are merely a proxy and can change
 	*/
-	return nil, nil
+	hashFnct := Utils.GetHashFunction()
+	Utils.ChordRoutingChannel.ReqCh <- hashFnct(key)
+	return <-Utils.ChordRoutingChannel.RespCh
 }
 
 func (c *Client) PutKey(ctx context.Context, msg *pb.PutKeyMsg) (*pb.PutKeyResp, error) {
-	/*
-	TODO: Check that the request is a legit request for the node, else we forward it to somewhere else
-	TODO: Implement replication to slave nodes
-	*/
+	var isSuccess bool
 	glog.Info("Received request to PUT key")
-	Utils.PutKeyChannel.ReqCh <- msg
-	isSuccess := <-Utils.PutKeyChannel.RespCh
+	_ = c.locate(msg.Key)
+	if false {
+		// TODO: Block to route request to other nodes if CHORD tells us a new address, once @Johnfiesten makes a new PR
+		isSuccess = true
+	} else {
+		// TODO: Implement replication to slave nodes before reporting success
+		Utils.PutKeyChannel.ReqCh <- msg
+		isSuccess = <-Utils.PutKeyChannel.RespCh
+	}
 	return &pb.PutKeyResp{Ack:isSuccess}, nil
 }
 
 func (c *Client) GetKey(ctx context.Context, msg *pb.GetKeyMsg) (*pb.GetKeyResp, error) {
 	/*
-	TODO: Check that the request is a legit request for the node, else we forward it to somewhere else
 	TODO: Implement get key request if slave, else forward key to slave if leader
 	*/
 	glog.Info("Received request to GET key")
+	_ = c.locate(msg.Key)
+	if false {
+		// TODO: Block to route request to other nodes if CHORD tells us a new address, once @Johnfiesten makes a new PR
+	} else {
+
+	}
 	Utils.GetKeyChannel.ReqCh <- msg.Key
 	resp := <-Utils.GetKeyChannel.RespCh
 	return resp, nil
