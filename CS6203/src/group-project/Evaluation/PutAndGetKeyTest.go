fe54fbccc306249d2cf23bae2b6d899ed7694bb2
johan.kok <johan.kok@grabtaxi.com>
2019-11-15 00:21:38 +0800

    Fixing unnecessary creation go client bug - Do not create the client in the loop - Instead, create it before calling multiple puts for keys

--- CS6203/src/group-project/Evaluation/PutAndGetKeyTest.go
+++ CS6203/src/group-project/Evaluation/PutAndGetKeyTest.go
@@ -37,11 +37,11 @@ func main() {
 		glog.Error(err)
 	} else {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(pollTimeOutMs) * time.Millisecond)
+		client := pb.NewPutKeyServiceClient(conn)
 		defer conn.Close(); defer cancel()
 
 		for key := 0; key < attempts; key++ {
 			glog.Infof(fmt.Sprintf("Attempting put key request - key: %d, val: %d", key, key))
-			client := pb.NewPutKeyServiceClient(conn)
 			if resp, err := client.PutKey(ctx, &pb.PutKeyMsg{Key: strconv.Itoa(key),
 				Val: []byte(strconv.Itoa(key))}); err != nil {
 				panic(err)
@@ -55,10 +55,10 @@ func main() {
 		glog.Error(err)
 	} else {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(pollTimeOutMs) * time.Millisecond)
+		client := pb.NewGetKeyServiceClient(conn)
 		defer conn.Close(); defer cancel()
 
 		for key := 0; key < attempts; key++ {
-			client := pb.NewGetKeyServiceClient(conn)
 			if resp, err := client.GetKey(ctx, &pb.GetKeyMsg{Key: strconv.Itoa(key)}); err != nil {
 				panic(err)
 			} else if resp.Ack != true {
