5a934b97f7e0e055b8fcf60667a0287ae81de44e
Johnfietsen <lucstefelmanns@live.nl>
2019-11-17 14:46:16 +0800

    push to debug

--- CS6203/src/group-project/Evaluation/KillLeaderTest.go
+++ CS6203/src/group-project/Evaluation/KillLeaderTest.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 	"time"
 	// "os"
-	"context"
-	"strconv"
+	// "context"
+	// "strconv"
 	"github.com/golang/glog"
-	"google.golang.org/grpc"
+	// "google.golang.org/grpc"
 	"group-project/Services/Client"
 	"group-project/Services/DB"
 	"group-project/Services/Election"
 	"group-project/Services/Raft"
 	"group-project/Services/Chord"
 	dep "group-project/Utils"
-	pb "group-project/Protobuf/Generate"
+	// pb "group-project/Protobuf/Generate"
 	"math/rand"
 	"sync"
 )
@@ -38,49 +38,49 @@ func putKeys(attempts int, pollTimeOutMs int, bootstrap_url string,
 
 	// start := time.Now()
 
-	if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_url,
-							bootstrap_port), grpc.WithInsecure()); err != nil {
-			glog.Error(err)
-	} else {
-		ctx, cancel := context.WithTimeout(context.Background(),
-								time.Duration(pollTimeOutMs) * time.Millisecond)
-		client := pb.NewPutKeyServiceClient(conn)
-		defer conn.Close(); defer cancel()
-
-		for key := 0; key < attempts; key++ {
-			glog.Infof(fmt.Sprintf("Attempting put key request - key: %d, val: %d", key, key))
-			if resp, err := client.PutKey(ctx, &pb.PutKeyMsg{Key: strconv.Itoa(key),
-				Val: []byte(strconv.Itoa(key))}); err != nil {
-				panic(err)
-			} else if resp.Ack != true {
-				glog.Error("Failed to insert key: ", key)
-			}
-		}
-	}
-
-	// time.Sleep(1 * time.Second)
-
-	if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_replica_url,
-					bootstrap_replica_port), grpc.WithInsecure()); err != nil {
-		glog.Error(err)
-	} else {
-		ctx, cancel := context.WithTimeout(context.Background(),
-								time.Duration(pollTimeOutMs) * time.Millisecond)
-		client := pb.NewGetKeyServiceClient(conn)
-		defer conn.Close(); defer cancel()
-
-		for key := 0; key < attempts; key++ {
-			if resp, err := client.GetKey(ctx,
-							&pb.GetKeyMsg{Key: strconv.Itoa(key)}); err != nil {
-				panic(err)
-			} else if resp.Ack != true {
-				glog.Error("Failed to retrieve key: ", key)
-			} else {
-				glog.Infof(fmt.Sprintf("Retrieved key: %d, val: %s", key,
-										string(resp.Val)))
-			}
-		}
-	}
+	// if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_url,
+	// 						bootstrap_port), grpc.WithInsecure()); err != nil {
+	// 		glog.Error(err)
+	// } else {
+	// 	ctx, cancel := context.WithTimeout(context.Background(),
+	// 							time.Duration(pollTimeOutMs) * time.Millisecond)
+	// 	client := pb.NewPutKeyServiceClient(conn)
+	// 	defer conn.Close(); defer cancel()
+	//
+	// 	for key := 0; key < attempts; key++ {
+	// 		glog.Infof(fmt.Sprintf("Attempting put key request - key: %d, val: %d", key, key))
+	// 		if resp, err := client.PutKey(ctx, &pb.PutKeyMsg{Key: strconv.Itoa(key),
+	// 			Val: []byte(strconv.Itoa(key))}); err != nil {
+	// 			panic(err)
+	// 		} else if resp.Ack != true {
+	// 			glog.Error("Failed to insert key: ", key)
+	// 		}
+	// 	}
+	// }
+	//
+	// // time.Sleep(1 * time.Second)
+	//
+	// if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_replica_url,
+	// 				bootstrap_replica_port), grpc.WithInsecure()); err != nil {
+	// 	glog.Error(err)
+	// } else {
+	// 	ctx, cancel := context.WithTimeout(context.Background(),
+	// 							time.Duration(pollTimeOutMs) * time.Millisecond)
+	// 	client := pb.NewGetKeyServiceClient(conn)
+	// 	defer conn.Close(); defer cancel()
+	//
+	// 	for key := 0; key < attempts; key++ {
+	// 		if resp, err := client.GetKey(ctx,
+	// 						&pb.GetKeyMsg{Key: strconv.Itoa(key)}); err != nil {
+	// 			panic(err)
+	// 		} else if resp.Ack != true {
+	// 			glog.Error("Failed to retrieve key: ", key)
+	// 		} else {
+	// 			glog.Infof(fmt.Sprintf("Retrieved key: %d, val: %s", key,
+	// 									string(resp.Val)))
+	// 		}
+	// 	}
+	// }
 
 	// elapsed := time.Since(start)
 	// n3, _ := file.WriteString(fmt.Sprint(elapsed, ","))
@@ -97,14 +97,17 @@ func main(){
 	dbCli, _ := dep.InitRocksDB(dep.GetEnvStr("STORAGE_LOC","./storage/leader"))
 
 	ports := []int{8000, 8002, 8004, 8006, 8008}
-	hashes := []int{2, 4, 6, 8, 10}
+	hashes := []int{1, 1, 1, 1, 1}
 	glog.Infof(fmt.Sprint("Used ports:", ports))
 	glog.Infof(fmt.Sprint("Used hashes:", hashes))
 	pollTimeOutMs := 6000000
 
 	var wg sync.WaitGroup
+	quitChannel := make(map[int](chan bool))
 
 	for i, p := range(ports) {
+
+		quitChannel[p] = make(chan bool)
 
 		wg.Add(1)
 
@@ -120,7 +123,15 @@ func main(){
 		go Client.Client{NodeAddr: nodeAddr, NodePort: nodePort + 1}.Start()
 
 		// Start up server to register all gRPC services
-		go Raft.Server{NodeAddr: nodeAddr, NodePort: nodePort}.Start()
+		go func(){
+			switch {
+			case condition:
+
+			}
+
+			Raft.Server{NodeAddr: nodeAddr, NodePort: nodePort}.Start()
+		}
+
 
 		// Start up state manager
 		go Election.ElectionManager{NodeAddr: nodeAddr, NodePort: nodePort,
