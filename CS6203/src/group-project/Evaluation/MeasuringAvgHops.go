213accda727ef27e64bb3ec5ff86106c40c1d0ee
johan.kok <johan.kok@grabtaxi.com>
2019-11-18 17:40:05 +0800

    Adding Measuring Avg Hops experiment

--- CS6203/src/group-project/Evaluation/MeasuringAvgHops.go
+++ CS6203/src/group-project/Evaluation/MeasuringAvgHops.go
@@ -26,9 +26,10 @@ func main() {
 	// TODO: Do not let client connect with a defined URL:PORT. Create a service that queries ZK for the details
 	// Leader address
 	bootstrap_url 	:= dep.GetEnvStr("REGISTER_LISTENER_DNS", "localhost")
-	bootstrap_port 	:= uint32(dep.GetEnvInt("REGISTER_LISTENER_PORT", 8001))
+	//bootstrap_port 	:= uint32(dep.GetEnvInt("REGISTER_LISTENER_PORT", 8001))
+	bootstrap_ports 	:= []uint32{8001, 8101, 8201, 8301, 8401, 8501}
 
-	const attempts 	= 1000
+	const attempts 	= 500
 	pollTimeOutMs 	:= 100000000000000
 
 	var totalHops int32 = 0
@@ -38,24 +39,26 @@ func main() {
 		- The leader is expected to route keys at random to the other severs
 		- Client measures average number of hops
 	*/
-	if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_url, bootstrap_port), grpc.WithInsecure()); err != nil {
-		glog.Error(err)
-	} else {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(pollTimeOutMs) * time.Millisecond)
-		client := pb.NewPutKeyServiceClient(conn)
-		defer conn.Close(); defer cancel()
+	for _, bootstrap_port := range(bootstrap_ports) {
+		if conn, err := grpc.Dial(fmt.Sprintf("%s:%d", bootstrap_url, bootstrap_port), grpc.WithInsecure()); err != nil {
+			glog.Error(err)
+		} else {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(pollTimeOutMs) * time.Millisecond)
+			client := pb.NewPutKeyServiceClient(conn)
+			defer conn.Close(); defer cancel()
 
-		for key := 0; key < attempts; key++ {
-			glog.Infof(fmt.Sprintf("Attempting put key request - key: %d, val: %d", key, key))
-			if resp, err := client.PutKey(ctx, &pb.PutKeyMsg{Key: strconv.Itoa(key),
-				Val: []byte(strconv.Itoa(key))}); err != nil {
-				panic(err)
-			} else if resp.Ack != true {
-				glog.Error("Failed to insert key: ", key)
-			} else {
-				totalHops += resp.Stats.NoOfHops
+			for key := 0; key < attempts; key++ {
+				glog.Infof(fmt.Sprintf("Attempting put key request - key: %d, val: %d", key, key))
+				if resp, err := client.PutKey(ctx, &pb.PutKeyMsg{Key: strconv.Itoa(key),
+					Val: []byte(strconv.Itoa(key))}); err != nil {
+					panic(err)
+				} else if resp.Ack != true {
+					glog.Error("Failed to insert key: ", key)
+				} else {
+					totalHops += resp.Stats.NoOfHops
+				}
 			}
 		}
 	}
-	glog.Info("Hops: ", float64(totalHops) / float64(attempts))
+	glog.Info("Hops: ", float64(totalHops) / float64(attempts * len(bootstrap_ports)))
 }
