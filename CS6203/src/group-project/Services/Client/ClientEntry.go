515f7d9698264a84171772b95d46ed962dfa5fb3
johan.kok <johan.kok@grabtaxi.com>
2019-11-22 16:55:44 +0800

    Added quick hack to resolve the problem of conflicting addresses for client to listen to and to be registered over in ZK - This might arise owing to the fact that clients need to listen to a different address than what they register in ZK - might be the case when we are deploying in Kubernetes or Docker compose, to which we cannot use local host to reach the client, yet the client has to listen to localhost - Client listening and registering ports are still the same, so those will not be touched

--- CS6203/src/group-project/Services/Client/ClientEntry.go
+++ CS6203/src/group-project/Services/Client/ClientEntry.go
@@ -5,6 +5,7 @@ import (
 	"github.com/golang/glog"
 	"google.golang.org/grpc"
 	pb "group-project/Protobuf/Generate"
+	dep "group-project/Utils"
 	"net"
 )
 
@@ -16,9 +17,17 @@ type Client struct {
 func (c Client) Start() {
 	/*
 		Starts the RAFT GRPC server that listens to incoming requests
+
+		TODO: 	Do not hard code localhost into server start :)- .... but no time to fix this :'(
+				Hard coding hack done as I just realized that clients have to listen to localhost but register their own address
+				differently in ZK when we use other deployment methods such as Docker Compose or Kubenetes
+				The difference between LISTENER_ADR and REGISTER_LISTENER_DNS environment variables is as follows:
+				- REGISTER_LISTENER_DNS: Address to be registered into ZK for discovery
+				- LISTENER_ADR: Address that clients subscribe to to listen to requests
 	*/
-	glog.Infof("Starting client at %s:%d", c.NodeAddr, c.NodePort)
-	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", c.NodeAddr, c.NodePort))
+	registerAddr := dep.GetEnvStr("LISTENER_ADR", c.NodeAddr)
+	glog.Infof("Starting client at %s:%d", registerAddr, c.NodePort)
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", registerAddr, c.NodePort))
 	if err != nil {glog.Fatal(err); panic(err)}
 
 	grpcServer := grpc.NewServer()
